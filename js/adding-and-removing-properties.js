460f030862c8833daad46f7957df9f751c6819e4
WillJHaggard <willjhaggard@gmail.com>
2015-10-31 18:44:45 -0400

    Added adding-and-removing-properties.js functionality

---
+++ js/adding-and-removing-properties.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+    // Set up the object
+    var hotel = {
+        name: 'Park',
+        rooms: 120,
+        booked: 77
+    };
+
+    hotel.gym = true;
+    hotel.pool = false;
+    delete hotel.booked;
+
+    //Update the HTML
+    var elName = document.getElementById('hotelName'); // Get the element
+    elName.textContent = hotel.name; // Update HTML with property of the object
+
+    var elPool = document.getElementById('pool'); // Get element
+    elPool.className = hotel.pool; // Update HTML with property of the object
+
+    var elGym = document.getElementById('gym'); // Get element
+    elGym.className = hotel.gym; // Update HTML
+
+
+
+
+
+}) ();
